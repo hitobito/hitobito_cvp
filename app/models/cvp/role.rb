68245571f7b25264d47de0fe42a9a159ab02e4f0
Andreas Maierhofer <maierhofer@puzzle.ch>
2020-11-30 16:58:37 +0100

    Unsure - Change to role

--- app/models/cvp/role.rb
+++ app/models/cvp/role.rb
@@ -11,6 +11,19 @@ module Cvp::Role
 
   included do
     before_validation :assert_consistent_roles_within_mitglieder_group
+    alias_method_chain :to_s, :merkmal
+  end
+
+  def to_s_with_merkmal(format=:default)
+    model_name = self.class.label
+    model_name = 'Merkmal' if self.class.sti_name.ends_with?('Merkmal')
+    string = label? ? "#{model_name} (#{label})" : model_name
+    if format == :long
+      I18n.t('activerecord.attributes.role.string_long', role: string, group: group.to_s)
+    else
+      string
+    end
+
   end
 
   class_methods do
