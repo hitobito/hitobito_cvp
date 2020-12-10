4af4ab6c8eb11ac6434ca061333bfdcf1b7aa078
Andreas Maierhofer <maierhofer@puzzle.ch>
2020-12-10 13:58:55 +0100

    Adjust specs as only present groups are returned

--- spec/om/structure/groups_spec.rb
+++ spec/om/structure/groups_spec.rb
@@ -6,17 +6,21 @@ describe Structure::Groups do
     path = File.join(File.dirname(__FILE__), '../../../lib/om/import/config.yml')
     YAML.load_file(path).deep_symbolize_keys
   }
-  let(:rows) { [Structure::GroupRow.new(1, 'CVP Schweiz', nil),
-                Structure::GroupRow.new(29, 'Kantonalparteien', 1),
-                Structure::GroupRow.new(159, 'CVP SG', 29)] }
+  let(:rows) { [
+    Structure::GroupRow.new(1, 'CVP Schweiz', nil),
+    Structure::GroupRow.new(29, 'Kantonalparteien', 1),
+    Structure::GroupRow.new(159, 'CVP SG', 29),
+    Structure::GroupRow.new(161, 'Mitgliedschaften', 159),
+  ] }
 
 
   it 'runs all operations' do
     expect(subject).to receive(:rows).and_return(rows)
 
     result = subject.build
-    expect(result).to have(2).items
+    expect(result).to have(3).items
     expect(result.first.type).to eq 'Bund'
     expect(result.second.type).to eq 'Kanton'
+    expect(result.third.type).to eq 'KantonMitglieder'
   end
 end
